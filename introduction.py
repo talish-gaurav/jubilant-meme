a451e91418382399ed4960178a4de66de79f458e
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-10-16 19:05:41 +0100

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,24 @@
+print(10, end="")
+import keyword
+
+print("Talish")
+
+print(10+9)
+
+print("My name is Talish",0, sep="_")
+
+print('''The tale of two cities
+      
+            NKWQJNCKJCN
+        DWKNLN''')
+
+a=10
+print("value", a)
+
+b=25
+print("value", b)
+
+name=input("Enter your name")
+print("welcome", name)
+
+print("keywords are:", keyword.kwlist)
